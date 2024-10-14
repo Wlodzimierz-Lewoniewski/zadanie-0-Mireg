39fe91935c05994ca543fd333c6b46f758ef25e0
Jakub M <k.miranowski@gmail.com>
2024-10-14 20:18:48 +0200

    first attempt

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
-wyraz=input()
+import re
 
-#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
-#To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
-wyraz=wyraz.strip()
+with open('input.txt', 'r') as file:
+    data = file.read().splitlines()
 
-#Wydruk na ekranie (w konsoli)
-print ('Ten wyraz został wprowadzony:', wyraz)
+docs_number = int(data[0])
+documents = data[1:docs_number+1]
+
+queries_number = int(data[docs_number+1])
+queries = data[docs_number+2:]
+
+counts = {}
+
+for query in queries:
+    pattern = re.compile(r"\b"+query+r"\b", re.IGNORECASE)
+    for i,doc in enumerate(documents):
+        matches = pattern.findall(doc)
+        counts[i] = len(matches)
+    #i - index of document, the program is suposed to return document number based on amount of time the query exist inside, in descending order.
+        counts_filtered = {key: value for key, value in counts.items() if value != 0}
+    
+    docs_order = dict(sorted(counts_filtered.items(), key=lambda item: item[1], reverse=True))
+    print(list(docs_order.keys()))
